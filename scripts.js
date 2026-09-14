bcd0b548da979bea5ddc6b957b07acc64a7c822b
Abner Nteteubaka <anteteubaka@gmail.com>
2026-09-14 16:36:22 +0000

    Ajout des éléments de base pour le compteur interactif

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const counterDisplay = document.getElementById("counter")
+const decreaseButton = document.getElementById("decrease")
+const increaseButton = document.getElementById("increase")
+const resetButton = document.getElementById("reset")
+
