4802a87d57b013e946f431ad3060756d961c5061
Abner Nteteubaka <anteteubaka@gmail.com>
2026-09-14 16:37:27 +0000

    Correction de l'espacement dans le fichier scripts.js

--- scripts.js
+++ scripts.js
@@ -2,4 +2,3 @@ const counterDisplay = document.getElementById("counter")
 const decreaseButton = document.getElementById("decrease")
 const increaseButton = document.getElementById("increase")
 const resetButton = document.getElementById("reset")
-
