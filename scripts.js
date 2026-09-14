fc86bfece8c051ae0aa2957833b62761dc6b02e7
Abner Nteteubaka <anteteubaka@gmail.com>
2026-09-14 16:38:27 +0000

    Ajout de la fonctionnalité de décrémentation du compteur

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@ const counterDisplay = document.getElementById("counter")
 const decreaseButton = document.getElementById("decrease")
 const increaseButton = document.getElementById("increase")
 const resetButton = document.getElementById("reset")
+
+
+let counter = 0;
+
+decreaseButton.addEventListener("click",
+    () => {
+        counter --;
+        counterDisplay.textContent = counter
+    }
+)
